5ef66dcb253df5bfb3d0518c7a04aee4500c06d1
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-11-16 15:00:43 +0100

    Stupid Coach Completed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+
+    if @question.include? '?'
+      @reply = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @reply = 'Great'
+    else
+      @reply = "I don't care, get dressed and go to work!"
+    end
+    return @reply
+  end
+end
